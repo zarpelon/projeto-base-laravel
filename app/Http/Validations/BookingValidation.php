e5ed283b441a4da8665a33c2162246057262793e
Diogo Zarpelon <zarpelon@gmail.com>
2019-12-15 06:08:23 -0300

    Removido código desnecessário, adicionado comentário das validações negociais

--- app/Http/Validations/BookingValidation.php
+++ app/Http/Validations/BookingValidation.php
@@ -30,7 +30,7 @@ class BookingValidation implements IValidation {
      */
     public function validate(array $data, $id = 0) 
     {
-        # Validação para não permitir que as datas sejam menores que a data atual
+        # Validação negocial para não permitir que o mesmo usuário reserve alguma sala mais de uma vez
         $from_date = \DateTime::createFromFormat('d/m/Y H:i:s', $data['from_date']);
 
         $user_bookings = $this->model
@@ -41,6 +41,7 @@ class BookingValidation implements IValidation {
             return ['success' => false, 'msg' => 'Já existe uma reserva de salas para seu usuário nesse período.'];
         }
 
+        # Validação negocial para não permitir que possa realizar mais de uma reserva para a mesma sala no mesmo período
         $bookings = $this->model
                          ->whereRoomId($data['room_id'])
                          ->whereRaw("'".$from_date->format('Y-m-d H:i:s')."' BETWEEN from_date AND to_date");
@@ -50,26 +51,5 @@ class BookingValidation implements IValidation {
         }
 
         return ['success' => true];
-/*
-        if ($validaDatas !== true) {
-            return ResponseHelper::responseError([], $validaDatas->first(), false);
-        }
-
-        # Valida em novos cadastros se não existe uma conferência para a mesma cidade.
-        if (empty($dados['id'])) {
-            if ($dados['tipo'] == 1) {
-                if ($this->conferenciaRepository->verificarMinCidade($dados['cidades']) == false) {
-                    return ResponseHelper::responseError([], 'É necessário informar ao menos 2 municípios para Conferência Intermunicipal!', false);
-                }
-            }
-
-            $validaDuplicidade = $this->conferenciaRepository->verificarDuplicidade($dados['cidades']);
-            if (count($validaDuplicidade) > 0) {
-                return ResponseHelper::responseError([], 'Já existe uma Conferência cadastrada para '.$validaDuplicidade->implode(', '), false);
-            }
-        }
-
-        return ResponseHelper::responseSuccess([], '', false);
-    */
     }
 }
